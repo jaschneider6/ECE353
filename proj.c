815815c7b25e4c82af03238da1185495e4b6700d
randrasco <64550800+randrasco@users.noreply.github.com>
2020-05-03 19:56:09 -0500

    Add files via upload

--- proj.c
+++ proj.c
@@ -6,6 +6,8 @@ volatile uint16_t PLAYER_X_COORD = 50;
 volatile uint16_t PLAYER_Y_COORD = 40;
 volatile bool ALERT_ASTEROID = true;
 volatile bool ALERT_PLAYER = true;
+volatile bool button;
+volatile uint16_t pColor;
 char STUDENT_NAME[] = "Ryan Andrasco/Jonathan Schneider";
 
 typedef struct
@@ -15,6 +17,7 @@ typedef struct
     int left;
     int right;
 } Rectangle;
+
 
 //*****************************************************************************
 // Determines if any part of the image would be off the screen if the image
@@ -156,13 +159,314 @@ void init_hardware(void)
   lcd_config_gpio();
   lcd_config_screen();
   lcd_clear_screen(LCD_COLOR_BLACK);
-  ps2_initialize();
   
   // Update the Space Shipt 60 times per second.
   gp_timer_config_32(TIMER2_BASE,TIMER_TAMR_TAMR_PERIOD, 1000000, false, true);
   gp_timer_config_32(TIMER3_BASE,TIMER_TAMR_TAMR_PERIOD, 500000, false, true);
   gp_timer_config_32(TIMER4_BASE,TIMER_TAMR_TAMR_PERIOD, 50000, false, true);
 }
+
+//volatile uint16_t B1_X = 0;
+//volatile uint16_t B1_Y = 0;
+//volatile PS2_DIR_t B1_Dir = PS2_DIR_CENTER;
+//volatile bool B1_rdy = true;
+//volatile uint16_t B2_X = 0;
+//volatile uint16_t B2_Y = 0;
+//volatile PS2_DIR_t B2_Dir = PS2_DIR_CENTER;
+//volatile bool B2_rdy = true;
+//volatile uint16_t B3_X = 0;
+//volatile uint16_t B3_Y = 0;
+//volatile PS2_DIR_t B3_Dir = PS2_DIR_CENTER;
+//volatile bool B3_rdy = true;
+//volatile uint16_t nextB = 1;
+//volatile uint16_t kills = 0;
+//
+//void ast_shoot(){
+//	if ((B1_X >= (ASTEROID_X_COORD - AsteroidWidthPixels/2)) && (B1_X <= (ASTEROID_X_COORD + AsteroidWidthPixels/2))){
+//		if((B1_Y >= (ASTEROID_Y_COORD - AsteroidHeightPixels/2)) && (B1_Y <= (ASTEROID_Y_COORD + AsteroidHeightPixels/2))){
+//			lcd_draw_image(ASTEROID_X_COORD, AsteroidWidthPixels, ASTEROID_Y_COORD, AsteroidHeightPixels, AsteroidBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			kills++;
+//			led_kill_count(kills);
+//			newAsteroid();//creates new asteroid
+//			return;
+//		}	
+//	}
+//	
+//	
+//	if ((B2_X >= (ASTEROID_X_COORD - AsteroidWidthPixels/2)) && (B2_X <= (ASTEROID_X_COORD + AsteroidWidthPixels/2))){
+//		if((B2_Y >= (ASTEROID_Y_COORD - AsteroidHeightPixels/2)) && (B2_Y <= (ASTEROID_Y_COORD + AsteroidHeightPixels/2))){
+//			lcd_draw_image(ASTEROID_X_COORD, AsteroidWidthPixels, ASTEROID_Y_COORD, AsteroidHeightPixels, AsteroidBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			kills++;
+//			led_kill_count(kills);
+//			newAsteroid();//creates new asteroid
+//			return;
+//		}	
+//	}
+//		
+//	
+//	if ((B3_X >= (ASTEROID_X_COORD - AsteroidWidthPixels/2)) && (B3_X <= (ASTEROID_X_COORD + AsteroidWidthPixels/2))){
+//		if((B3_Y >= (ASTEROID_Y_COORD - AsteroidHeightPixels/2)) && (B3_Y <= (ASTEROID_Y_COORD + AsteroidHeightPixels/2))){
+//			lcd_draw_image(ASTEROID_X_COORD, AsteroidWidthPixels, ASTEROID_Y_COORD, AsteroidHeightPixels, AsteroidBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			kills++;
+//			led_kill_count(kills);
+//			newAsteroid();//creates new asteroid
+//			return;
+//		}	
+//	}
+//	return;
+//}
+//	
+//
+////***********
+////bullet updater
+////***********
+//
+//
+//void BulletUpdater(){
+//	switch(B1_Dir){
+//		case PS2_DIR_DOWN:
+//		if(B1_Y < 320){
+//			B1_Y++;
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B1_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_UP:
+//		if(B1_Y > 0){
+//			B1_Y--;
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B1_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_LEFT:
+//		if(B1_X > 0){
+//			B1_X--;
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B1_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_RIGHT:
+//		if(B1_X < 240){
+//			B1_X++;
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B1_X, 1, B1_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B1_rdy = true;
+//		}
+//		break;
+//		
+//		default:
+//			break;
+//	}
+//	
+//	
+//		switch(B2_Dir){
+//		case PS2_DIR_DOWN:
+//		if(B2_Y < 320){
+//			B2_Y++;
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B2_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_UP:
+//		if(B2_Y > 0){
+//			B2_Y--;
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B2_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_LEFT:
+//		if(B2_X > 0){
+//			B2_X--;
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B2_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_RIGHT:
+//		if(B2_X < 240){
+//			B2_X++;
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B2_X, 1, B2_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B2_rdy = true;
+//		}
+//		break;
+//		
+//		default:
+//			break;
+//	}
+//	
+//		switch(B3_Dir){
+//		case PS2_DIR_DOWN:
+//		if(B3_Y < 320){
+//			B3_Y++;
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B3_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_UP:
+//		if(B3_Y > 0){
+//			B3_Y--;
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B3_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_LEFT:
+//		if(B3_X > 0){
+//			B3_X--;
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B3_rdy = true;
+//		}
+//		break;
+//		
+//		case PS2_DIR_RIGHT:
+//		if(B3_X < 240){
+//			B3_X++;
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, pColor, LCD_COLOR_BLACK);
+//		} else{
+//			lcd_draw_image(B3_X, 1, B3_Y, 1, BulletBitmaps, LCD_COLOR_BLACK, LCD_COLOR_BLACK);
+//			B3_rdy = true;
+//		}
+//		break;
+//		
+//		default:
+//			break;
+//	}
+//	
+//	
+//}
+//
+//
+////****
+////Shootbullet
+////****
+//void Shootbullet(PS2_DIR_t dir) {
+//	
+//	if(B1_rdy){
+//		switch (dir){
+//			case up:
+//				B1_X = PLAYER_X_COORD;
+//				B1_Y = PLAYER_Y_COORD - PlayerHeightPixels/2;
+//				B1_Dir = PS2_DIR_UP;
+//				B1_rdy = false;
+//				break;
+//			case down:
+//				B1_X = PLAYER_X_COORD;
+//				B1_Y = PLAYER_Y_COORD + PlayerHeightPixels/2;
+//				B1_Dir = PS2_DIR_DOWN;
+//				B1_rdy = false;
+//				break;
+//			case left:
+//				B1_X = PLAYER_X_COORD - PlayerWidthPixels/2;
+//				B1_Y = PLAYER_Y_COORD;
+//				B1_Dir = PS2_DIR_LEFT;
+//				B1_rdy = false;
+//				break;
+//			case right:
+//				B1_X = PLAYER_X_COORD + PlayerWidthPixels/2;
+//				B1_Y = PLAYER_Y_COORD;
+//				B1_Dir = PS2_DIR_RIGHT;
+//				B1_rdy = false;
+//				break;
+//			default:
+//				break;
+//		}
+//	}
+//	
+//	else if(B2_rdy){
+//		switch (dir){
+//			case up:
+//				B2_X = PLAYER_X_COORD;
+//				B2_Y = PLAYER_Y_COORD - PlayerHeightPixels/2;
+//				B2_Dir = PS2_DIR_UP;
+//				B2_rdy = false;
+//				break;
+//			case down:
+//				B2_X = PLAYER_X_COORD;
+//				B2_Y = PLAYER_Y_COORD + PlayerHeightPixels/2;
+//				B2_Dir = PS2_DIR_DOWN;
+//				B2_rdy = false;
+//				break;
+//			case left:
+//				B2_X = PLAYER_X_COORD - PlayerWidthPixels/2;
+//				B2_Y = PLAYER_Y_COORD;
+//				B2_Dir = PS2_DIR_LEFT;
+//				B2_rdy = false;
+//				break;
+//			case right:
+//				B2_X = PLAYER_X_COORD + PlayerWidthPixels/2;
+//				B2_Y = PLAYER_Y_COORD;
+//				B2_Dir = PS2_DIR_RIGHT;
+//				B2_rdy = false;
+//				break;
+//			default:
+//				break;
+//		}
+//	}
+//	
+//	else if(B3_rdy){
+//		switch (dir){
+//			case up:
+//				B3_X = PLAYER_X_COORD;
+//				B3_Y = PLAYER_Y_COORD - PlayerHeightPixels/2;
+//				B3_Dir = PS2_DIR_UP;
+//				B3_rdy = false;
+//				break;
+//			case down:
+//				B3_X = PLAYER_X_COORD;
+//				B3_Y = PLAYER_Y_COORD + PlayerHeightPixels/2;
+//				B3_Dir = PS2_DIR_DOWN;
+//				B3_rdy = false;
+//				break;
+//			case left:
+//				B3_X = PLAYER_X_COORD - PlayerWidthPixels/2;
+//				B3_Y = PLAYER_Y_COORD;
+//				B3_Dir = PS2_DIR_LEFT;
+//				B3_rdy = false;
+//				break;
+//			case right:
+//				B3_X = PLAYER_X_COORD + PlayerWidthPixels/2;
+//				B3_Y = PLAYER_Y_COORD;
+//				B3_Dir = PS2_DIR_RIGHT;
+//				B3_rdy = false;
+//				break;
+//			default:
+//				break;
+//		}
+//	}
+//	
+//	
+//	
+//	
+//}
+	
+	
 
 //*****************************************************************************
 // Main application for HW3
@@ -175,10 +479,16 @@ void proj_main(void)
 
       while(!game_over)
       {
+					if(button){
+						button = false;
+						lcd_draw_rectangle_centered(100, 10, 100, 10, LCD_COLOR_BLUE2);
+					}
+						
+				led_kill_count(0xF);
           if(ALERT_ASTEROID)
           {
             ALERT_ASTEROID = false;
-            
+            lcd_draw_rectangle_centered(ASTEROID_X_COORD, AsteroidWidthPixels + 2, ASTEROID_Y_COORD, AsteroidHeightPixels + 2, LCD_COLOR_BLACK);
             lcd_draw_image(
                           ASTEROID_X_COORD,                       // X Center Point
                           AsteroidWidthPixels,   // Image Horizontal Width
@@ -204,15 +514,15 @@ void proj_main(void)
           if(ALERT_PLAYER)
           {
             ALERT_PLAYER = false;
-            
+            lcd_draw_rectangle_centered(PLAYER_X_COORD, PlayerWidthPixels + 2, PLAYER_Y_COORD, PlayerHeightPixels + 2, LCD_COLOR_BLACK);
             lcd_draw_image(
                           PLAYER_X_COORD,          // X Center Point
                           PlayerWidthPixels,       // Image Horizontal Width
                           PLAYER_Y_COORD,          // Y Center Point
                           PlayerHeightPixels,      // Image Vertical Height
                           PlayerBitmaps,           // Image
-                          pColor,            // Foreground Color
-                          LCD_COLOR_BLACK           // Background Color
+                          pColor,            			 // Foreground Color
+                          LCD_COLOR_BLACK          // Background Color
                         );
               
              game_over = check_game_over(
